f3d0a3f2d72fbd0ff4dd41618581b7e515fe2b50
d.kanai <contact@dkanai.com>
2022-07-09 16:42:55 +0900

    make it simple

--- src/main/java/com/example/restservice/SampleController.java
+++ src/main/java/com/example/restservice/SampleController.java
@@ -1,15 +1,18 @@
 package com.example.restservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
 
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
 
     @GetMapping("/hello")
     public String hello() {
-        System.out.println("/hello: 200");
+        logger.info("nice log");
         return "hello";
     }
 
